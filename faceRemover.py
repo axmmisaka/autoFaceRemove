b358f288e757acf4db643b3318973cc06a47bce2
Tachibana Taki <axmmisaka@users.noreply.github.com>
2018-04-06 18:44:36 +0800

    added random choose face feature

--- faceRemover.py
+++ faceRemover.py
@@ -18,31 +18,68 @@ def faceRemove(filename, path, maskpath, isFolder, inputName):
 
     allFaceEncodings = []
     allNames = []
-    faceCounter = 0;
+    faceCounter = 0
     for picname in files:
-        savename = path+picname.rpartition('.')[0]+"_masked."+picname.rpartition('.')[2]
+        #Add _masked before extension, after the original name. E.g. Taki.jpg will be Taki_masked.jpg
+        savename = path + picname.rpartition('.')[0] + "_masked." + picname.rpartition('.')[2]
         picture = face_recognition.load_image_file(path+picname)
         originalpic = Image.open(path+picname)
         originalpic.load()
         faceLocations = face_recognition.face_locations(picture)
         faceEncodings = face_recognition.face_encodings(picture, faceLocations)
         
-        #Duplication deletion
+        # Detect if current face has appeared
         for faceEncoding,faceLocation in zip(faceEncodings,faceLocations):
-            matchedFace = face_recognition.compare_faces(allFaceEncodings, faceEncoding)
-            #matchedFace is an numpy.array in a list. Why?
-            #It works now. Fuck!
-            if not (True in matchedFace):
-                print("I found one unknown face in file {0}.".format(picname))
-                #Not duplicated
+
+            if not randomizeFace:
+                matchedFace = face_recognition.compare_faces(allFaceEncodings, faceEncoding)
+                #matchedFace is an numpy.array in a list. Why?
+                #It works now. Fuck!
+                if not (True in matchedFace):
+                    print("I found one unknown face in file {0}.".format(picname))
+                    #Not duplicated
+                    faceCounter += 1
+
+                    # Will minus 1 afterwards when using faceCounter, so no out of bound worries
+                    if faceCounter > len(masklist):
+                        print("Insufficent masks. Exiting...")
+                        exit()
+
+                    allFaceEncodings.append(faceEncoding)
+
+                    if inputName:
+                        top, right, bottom, left = faceLocation
+                        
+                        #Like in example, don't know why there's no error
+                        croppedImage = picture[top:bottom,left:right]
+                        
+                        pilImage = Image.fromarray(croppedImage)
+                        pilImage.show()
+                        
+                        print("Input the name of shown face.")
+                        name = input()
+                        allNames.append(name)
+                    else:
+                        # Since there's no name inputted, the code will be used for this person's name
+                        allNames.append(str(faceCounter))
+                    # use allFaceEncodings here because sometimes matchedFace is not initialized.
+                    faceCorrespondingMask = len(allFaceEncodings) - 1
+                    print("(DEBUG)I am using {} as the rage.".format(faceCorrespondingMask))
+                else:
+                    # This is the same as the offical example. As it must be an old face, this is totally ok unlike in line 60
+                    firstMatchIndex = matchedFace.index(True)
+                    existName = allNames[firstMatchIndex]
+                    print("I found an existing face in {0}. This person is probably{1}: ".format(picname, existName))
+                    faceCorrespondingMask = firstMatchIndex
+                    print("(DEBUG)I am using {} as the rage.".format(faceCorrespondingMask))
+            else:
                 faceCounter += 1
-
+                # Will minus 1 afterwards when using faceCounter, so no out of bound worries
                 if faceCounter > len(masklist):
-                    print("Insufficent masks. Exiting...")
-                    exit()
-
-                allFaceEncodings.append(faceEncoding)
-
+                        print("Insufficent masks. Exiting...")
+                        exit()
+                faceCorrespondingMask = faceCounter - 1
+                #Copied from top
                 if inputName:
                     top, right, bottom, left = faceLocation
                     
@@ -57,18 +94,10 @@ def faceRemove(filename, path, maskpath, isFolder, inputName):
                     allNames.append(name)
                 else:
                     allNames.append(str(faceCounter))
-                # use allFaceEncodings here because sometimes matchedFace is not initialized.
-                faceCorrespondingMask = len(allFaceEncodings) - 1
-                print("(DEBUG)I am using {} as the rage.".format(faceCorrespondingMask))
-            else:
-                # This is the same as the offical example. As it must be an old face, this is totally ok unlike in line 60
-                firstMatchIndex = matchedFace.index(True)
-                existName = allNames[firstMatchIndex]
-                print("I found an existing face in {0} and I am ignoring it. This person is probably{1}: ".format(picname, existName))
-                faceCorrespondingMask = firstMatchIndex
-                print("(DEBUG)I am using {} as the rage.".format(faceCorrespondingMask))
+                print("I found a face in file {0}, which is No. {1} face in this run.".format(picname, str(faceCounter)))
+
             #mask every face
-            mask = Image.open(maskpath+"/"+masklist[faceCorrespondingMask])
+            mask = Image.open(maskpath + "/" + masklist[faceCorrespondingMask])
             coverFace(originalpic, faceLocation, mask)
         originalpic.save(savename)
 
@@ -80,18 +109,16 @@ def faceRemove(filename, path, maskpath, isFolder, inputName):
     return zip(allFaceEncodings,allNames)
 
 
-
-
-
 # Load arguments
 if __name__ == '__main__':
     try:
-        options, arguments = getopt.getopt(sys.argv[1:],"hni:f:m:",["help","inputname","input=","folder=","masks="])
+        options, arguments = getopt.getopt(sys.argv[1:],"hnri:f:m:",["help","inputname","random","input=","folder=","masks="])
     except getopt.GetoptError:
         print("Argument Error")
+        exit()
 
     #Process arguments
-    inputName = False;
+    inputName = False
     isFolder = True
     if len(options) == 0:
         print("Use --help for help.")
@@ -105,25 +132,27 @@ if __name__ == '__main__':
                 #If this is true, program will ask user to input name for every face found.
                 #Otherwise program will automaticlly assign numbers to these faces. 
                 inputName = True
+                print("Name inputting is enabled.")
             elif name in ("-i","--input"):
                 isFolder = False
                 filename = value
                 path = ""
+                print("Using file " + filename)
             elif name in ("-f","--folder"):
                 if (isFolder!= True):
                     print("Ignoring folder...")
                 else:
                     filename = value
                     path = value+"/"
+                    print("Using folder " + path)
             elif name in ("-m","--masks"):
                 maskpath = value
+            elif name in ("-r","--random"):
+                randomizeFace = True
+                print("Ignoring who the person is...")
+
     print("Initializing railgun, please wait...")
     import face_recognition
     print("Done.")
     result = faceRemove(filename, path, maskpath, isFolder, inputName)
     print(result)
-
-
-
-        
-
